9d61dd7f7b45a73967a83d9a43a7bd80472d57d4
Faruk KARAAASLAN <faruk.karaaslan.55@gmail.com>
2023-06-12 23:28:22 +0300

    feat(HotelProject.DataAcces/Abstract):All enttiy Interface created.

---
+++ ApiConsume/HotelProject.DataAccess/Abstract/IGenericDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccess.Abstract;
+
+public interface IGenericDal<TEntity> where TEntity : class
+{
+    void Insert(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    List<TEntity> GetAll();
+    TEntity GetByID(int id);
+}
---
+++ ApiConsume/HotelProject.DataAccess/Abstract/IServiceDal.cs
@@ -0,0 +1,12 @@
+﻿using HotelProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccess.Abstract;
+
+public interface IServiceDal: IGenericDal<Services>
+{
+}
