c3c377395f7f730e06aaf8d9d87192bf16859756
Faruk KARAAASLAN <faruk.karaaslan.55@gmail.com>
2023-06-12 23:18:12 +0300

    feat(HotelProject.DataAcces/Concrete): Project data base connection created.

---
+++ ApiConsume/HotelProject.DataAccess/Concrete/ProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using HotelProject.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccess.Concrete;
+
+public class ProjectContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Hotel; Trusted_Connection=True;");
+    }
+    public DbSet<Room>  Rooms{ get; set; }
+    public DbSet<Services>  Services{ get; set; }
+    public DbSet<Staff>  Staffs{ get; set; }
+    public DbSet<Subscribe>  Subscribes{ get; set; }
+    public DbSet<Testimonial>  Testimonials{ get; set; }
+}
