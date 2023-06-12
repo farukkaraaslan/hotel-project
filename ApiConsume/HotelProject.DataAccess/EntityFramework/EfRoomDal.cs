69380d5a197ccda29a23678a68e55f710c4fc4c2
Faruk KARAAASLAN <faruk.karaaslan.55@gmail.com>
2023-06-12 23:42:12 +0300

    feat(HotelProject.DataAccess/EntityFramework): All enttiy EfDal created.

---
+++ ApiConsume/HotelProject.DataAccess/EntityFramework/EfRoomDal.cs
@@ -0,0 +1,18 @@
+﻿using HotelProject.DataAccess.Abstract;
+using HotelProject.DataAccess.Concrete;
+using HotelProject.DataAccess.Repositories;
+using HotelProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccess.EntityFramework;
+
+public class EfRoomDal : GenericRepository<Room>, IRoomDal
+{
+    public EfRoomDal(ProjectContext context) : base(context)
+    {
+    }
+}
---
+++ ApiConsume/HotelProject.DataAccess/EntityFramework/EfSubscribeDal.cs
@@ -0,0 +1,18 @@
+﻿using HotelProject.DataAccess.Abstract;
+using HotelProject.DataAccess.Concrete;
+using HotelProject.DataAccess.Repositories;
+using HotelProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccess.EntityFramework;
+
+public class EfSubscribeDal : GenericRepository<Subscribe>, ISubscribeDal
+{
+    public EfSubscribeDal(ProjectContext context) : base(context)
+    {
+    }
+}
