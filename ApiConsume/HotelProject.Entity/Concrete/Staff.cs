7c2799c6a667fe50091a133d0b9d3871a3fcb9a2
Faruk KARAAASLAN <faruk.karaaslan.55@gmail.com>
2023-06-12 22:50:16 +0300

    feat(HotelProject.Entity/Concrete): Subscribe entity created.

--- ApiConsume/HotelProject.Entity/Concrete/Staff.cs
+++ ApiConsume/HotelProject.Entity/Concrete/Staff.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HotelProject.Entity.Concrete
+namespace HotelProject.Entity.Concrete;
+
+public class Staff
 {
-    internal class Staff
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Title { get; set; }
+    public string  SocialMedia1 { get; set; }
+    public string SocialMedia2 { get; set; }
+    public string SocialMedia3 { get; set; }
 }
---
+++ ApiConsume/HotelProject.Entity/Concrete/Subscribe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.Entity.Concrete;
+
+public class Subscribe
+{
+    public int Id { get; set; }
+    public string Mail { get; set; }
+}
